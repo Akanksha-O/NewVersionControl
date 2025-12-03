a6b4f883bb8bb4c74c66bc1d6a6423b96ff31623
Akanksha <akankshaojha.info@gmail.com>
2025-12-03 14:17:40 +0530

    Initial commit

---
+++ application.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# MongoDB connection
+client = MongoClient("mongodb+srv://akankshaojhainfo_db_user:onuIZ2BzLw5lMIE5@cluster0.fj5y0vb.mongodb.net/?appName=Cluster0")
+db = client["todo_db"]
+collection = db["items"]
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo():
+    itemName = request.form.get("itemName")
+    itemDescription = request.form.get("itemDescription")
+
+    data = {
+        "itemName": itemName,
+        "itemDescription": itemDescription
+    }
+
+    collection.insert_one(data)
+
+    return jsonify({"message": "To-Do Item Stored Successfully!"})
